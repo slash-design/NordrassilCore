125e8998256104febd1b7a1a1bb3f0686eba2b74
slash-design <elite_slash@hotmail.com>
2026-01-01 22:00:53 +0100

    Core/Player: Rename quest sharing fields [skip ci]
    * Core/Player: Keep shared quests through map changes instead of getting stuck in busy state
    
    * remove logic that doesn't work

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -2038,11 +2038,12 @@ class Player : public Unit, public GridObject<Player>
         void ClearWorldQuest();
 		bool HasWorldQuestEnabled() const;
 
-        ObjectGuid GetDivider() { return m_divider; }
-        void SetDivider(ObjectGuid guid) { m_divider = guid; }
+        uint32 GetSharedQuestID() const { return m_sharedQuestId; }
+        ObjectGuid GetPlayerSharingQuest() const { return m_playerSharingQuest; }
+        void SetQuestSharingInfo(ObjectGuid guid, uint32 id) { m_playerSharingQuest = guid; m_sharedQuestId = id; }
+        void ClearQuestSharingInfo() { m_playerSharingQuest = ObjectGuid::Empty; m_sharedQuestId = 0; }
 
         uint32 GetInGameTime() { return m_ingametime; }
-
         void SetInGameTime(uint32 time) { m_ingametime = time; }
 
         void AddTimedQuest(uint32 quest_id) { m_timedquests.insert(quest_id); }
@@ -3308,7 +3309,8 @@ class Player : public Unit, public GridObject<Player>
         RewardedQuestSet m_accuntQuests;
         QuestStatusSaveMap m_RewardedQuestsSave;
 
-        ObjectGuid m_divider;
+        ObjectGuid m_playerSharingQuest;
+        uint32 m_sharedQuestId;
         uint32 m_ingametime;
 
         /*********************************************************/
