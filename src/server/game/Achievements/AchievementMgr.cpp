73a9283673b90c9783121278028dcfb81098ac7b
slash-design <elite_slash@hotmail.com>
2026-01-05 11:00:34 +0100

    Achievement: Updated to allow those with security to still get a realm first achievement, just not with GM active

--- src/server/game/Achievements/AchievementMgr.cpp
+++ src/server/game/Achievements/AchievementMgr.cpp
@@ -2038,9 +2038,8 @@ bool AchievementMgr<Player>::CanCompleteCriteria(AchievementEntry const* achieve
             return false;
 
         if (GetOwner())
-            if (GetOwner()->GetSession())
-                if (GetOwner()->GetSession()->GetSecurity())
-                    return false;
+            if (GetOwner()->isGameMaster())
+                return false;
     }
 
     return true;
