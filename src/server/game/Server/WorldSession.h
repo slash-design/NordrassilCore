0e8656ce53b1941475e3cd2257f9913deabfbbed
slash-design <elite_slash@hotmail.com>
2026-01-18 00:02:43 +0100

    feat(items): implement bags/bank/reagent auto-sort with stacking + child-item equip handling
    - Add Player::ApplyOnBagsItems, ApplyOnBankItems, ApplyOnReagentBankItems helpers to iterate container slots consistently.
    - Rework HandleSortBags/HandleSortBankBags/HandleSortReagentBankBags to:
    first attempt stacking/relocating items within the respective storage,
    then sort items by item level while keeping duplicates grouped.
    - Restore proper handling for ITEM_FLAG_CHILD during equip swaps (auto-unequip parent first).
    - Add early return for no-op swaps (src == dst) to avoid unnecessary processing.

--- src/server/game/Server/WorldSession.h
+++ src/server/game/Server/WorldSession.h
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
+ * This file is part of the DestinyCore Project. See AUTHORS file for Copyright information
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -1751,9 +1750,10 @@ class WorldSession
 
         void HandleSocketGems(WorldPackets::Item::SocketGems& packet);
 
-        void HandleSortBags(WorldPackets::Item::SortBags& packet);
-        void HandleSortBankBags(WorldPackets::Item::SortBankBags& packet);
-        void HandleSortReagentBankBags(WorldPackets::Item::SortReagentBankBags& packet);
+        // Sort Bags
+        void HandleSortBags(WorldPackets::Item::SortBags& sortBags);
+        void HandleSortBankBags(WorldPackets::Item::SortBankBags& sortBankBags);
+        void HandleSortReagentBankBags(WorldPackets::Item::SortReagentBankBags& sortReagentBankBags);
 
         void HandleCancelTempEnchantmentOpcode(WorldPackets::Item::CancelTempEnchantment& packet);
 
