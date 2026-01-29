870c136b4c04c929b9df029df8880d21d9051af8
slash-design <elite_slash@hotmail.com>
2026-01-29 21:51:11 +0100

    fix(scripts): fix Roiling Storm periodic hook DBC mismatch
    Roiling Storm (196290, 196296): bind OnEffectPeriodic for EFFECT_0 using SPELL_AURA_ANY to handle differing DBC aura types (226/23) and ensure the tick handler executes. Fixes “did not match dbc effect data” warnings.

--- src/server/scripts/Legion/EyeofAzshara/boss_serpentrix.cpp
+++ src/server/scripts/Legion/EyeofAzshara/boss_serpentrix.cpp
@@ -1,9 +1,19 @@
 /*
-    http://uwow.biz
-    Dungeon : Eye of Azshara 100-110
-    Encounter: Serpentrix
-    Normal: 100%, Heroic: 100%, Mythic: 100%
-*/
+ * This file is part of the DestinyCore Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "eye_of_azshara.h"
 
@@ -490,8 +500,7 @@ class spell_eye_of_azshara_roiling_storm : public AuraScript
 
     void Register() override
     {
-        OnEffectPeriodic += AuraEffectPeriodicFn(spell_eye_of_azshara_roiling_storm::OnTick, EFFECT_0, SPELL_AURA_PERIODIC_DUMMY);
-        OnEffectPeriodic += AuraEffectPeriodicFn(spell_eye_of_azshara_roiling_storm::OnTick, EFFECT_0, SPELL_AURA_PERIODIC_TRIGGER_SPELL);
+        OnEffectPeriodic += AuraEffectPeriodicFn(spell_eye_of_azshara_roiling_storm::OnTick, EFFECT_0, SPELL_AURA_ANY);
     }
 };
 
