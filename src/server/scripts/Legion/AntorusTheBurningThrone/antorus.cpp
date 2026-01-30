1e7c5d9a3f75c93e5905feaf95d3496687722c57
slash-design <elite_slash@hotmail.com>
2026-01-30 09:45:25 +0100

    fix(scripts): correct Bound by Fel hook effect index to match DBC
    Bound by Fel (252621): bind OnEffectApply/OnEffectPeriodic to the correct DBC effect (EFFECT_0 / periodic) instead of EFFECT_1. Fixes “did not match dbc effect data” warnings and ensures the tick/apply handlers execute.

--- src/server/scripts/Legion/AntorusTheBurningThrone/antorus.cpp
+++ src/server/scripts/Legion/AntorusTheBurningThrone/antorus.cpp
@@ -1,6 +1,19 @@
 /*
-    https://uwow.biz/
-*/
+ * This file is part of the DestinyCore Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "antorus.h"
 #include "QuestData.h"
@@ -271,7 +284,7 @@ class spell_atbt_bound_by_fel : public AuraScript
             return;
         }
 
-        if(second.IsEmpty())
+        if (second.IsEmpty())
             return;
 
         Unit* second_targ = ObjectAccessor::GetPlayer(*target, second);
@@ -306,8 +319,8 @@ class spell_atbt_bound_by_fel : public AuraScript
 
     void Register() override
     {
-        OnEffectPeriodic += AuraEffectPeriodicFn(spell_atbt_bound_by_fel::OnTick, EFFECT_1, SPELL_AURA_PERIODIC_DUMMY);
-        OnEffectApply += AuraEffectApplyFn(spell_atbt_bound_by_fel::OnApply, EFFECT_1, SPELL_AURA_PERIODIC_DUMMY, AURA_EFFECT_HANDLE_REAL);
+        OnEffectPeriodic += AuraEffectPeriodicFn(spell_atbt_bound_by_fel::OnTick, EFFECT_0, SPELL_AURA_PERIODIC_DUMMY);
+        OnEffectApply += AuraEffectApplyFn(spell_atbt_bound_by_fel::OnApply, EFFECT_0, SPELL_AURA_PERIODIC_DUMMY, AURA_EFFECT_HANDLE_REAL);
     }
 };
 
