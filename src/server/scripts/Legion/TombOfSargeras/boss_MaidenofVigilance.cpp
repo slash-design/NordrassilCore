63f61e3c278987663fa6f1840634865df9fd6c8f
slash-design <elite_slash@hotmail.com>
2026-01-30 09:25:58 +0100

    fix(scripts): fix Infusions periodic hook DBC mismatch
    Infusions (235213, 235240): bind OnEffectPeriodic for EFFECT_0 using SPELL_AURA_ANY to support differing DBC aura types (23/226) and ensure the tick handler executes. Fixes “did not match dbc effect data” warnings.

--- src/server/scripts/Legion/TombOfSargeras/boss_MaidenofVigilance.cpp
+++ src/server/scripts/Legion/TombOfSargeras/boss_MaidenofVigilance.cpp
@@ -1,6 +1,19 @@
 /*
-https://uwow.biz/
-*/
+ * This file is part of the DestinyCore Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "tomb_of_sargeras.h"
 
@@ -731,8 +744,7 @@ class spell_tos_infusions : public AuraScript
 
     void Register() override
     {
-        OnEffectPeriodic += AuraEffectPeriodicFn(spell_tos_infusions::OnTick, EFFECT_0, SPELL_AURA_PERIODIC_DUMMY);
-        OnEffectPeriodic += AuraEffectPeriodicFn(spell_tos_infusions::OnTick, EFFECT_0, SPELL_AURA_PERIODIC_TRIGGER_SPELL);
+        OnEffectPeriodic += AuraEffectPeriodicFn(spell_tos_infusions::OnTick, EFFECT_0, SPELL_AURA_ANY);
     }
 };
 
